13c13fbaff5aa7aea9303ccf71641e96a1d7105c
amuell <awm@posteo.de>
2018-04-29 22:06:57 +0200

    New audio files, Keyboard driven player, repeated keypresses restart sound.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,32 @@
 ﻿$(document).ready(function(){
-	// Remove no-javascript class if js is enabled
-	$("body.no-javascript").removeClass("no-javascript")
+    // Remove no-javascript class if js is enabled
+    $("body.no-javascript").removeClass("no-javascript")
 
-	$("audio").removeAttr("controls").each(function(i, audioElement) {
-	    var audio = $(this);
-	    var that = this;
-	    $("#doc").append($('<li><a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"><img src="img/' + audio.attr("class") + '.png"/></a></li>').click(function() {that.play();}));
-	});
 
+    $("audio").removeAttr("controls").each(function(i, audioElement) {
+        var audio = $(this);
+        var that = this;
+
+        $("#doc").append($('<tr><td id="td' + audio.attr("key") + '">' + audio.attr("key") + '</td><td><a id="el' + audio.attr("key") + '" href="#"><b>' + audio.attr("name") + '</b></a></td></tr>').click(function() {that.pause(); that.currentTime=0; that.play();}));
+    });
+
+});
+
+$(document).keypress(function (e) {
+
+    //console.log(String.fromCharCode(e.which));
+    $("audio").removeAttr("controls").each(function(i, audioElement) {
+        var audio = $(this);
+
+        //console.log(audio.attr("key"));
+
+        if (audio.attr("key") === String.fromCharCode(e.which)) {
+            $("#el" + audio.attr("key"))[0].click();
+            $("#td" + audio.attr("key"))[0].style = "background-color: #ff0000";
+            setTimeout(function() {
+                $("#td" + audio.attr("key"))[0].style = "background-color: #ffffff";
+            }, 400);
+        }
+
+    });
 });
